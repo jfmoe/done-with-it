83de29a745220b2c2dd602a0cea4bc62169ca447
jfmoe <taoxfan@outlook.com>
2024-03-27 20:37:02 +0800

    feat: add icon to ViewImageScreen

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
