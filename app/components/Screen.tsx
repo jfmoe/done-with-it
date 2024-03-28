9087e17546cee134010e9ae5083d1191db7948c3
jfmoe <taoxfan@outlook.com>
2024-03-28 10:36:08 +0800

    feat: create Screen component

---
+++ app/components/Screen.tsx
@@ -0,0 +1,19 @@
+import Constants from 'expo-constants';
+import { ReactNode } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+interface Props {
+  children: ReactNode;
+}
+
+const Screen = ({ children }: Props) => {
+  return <SafeAreaView style={styles.screen}>{children}</SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+  },
+});
+
+export default Screen;
