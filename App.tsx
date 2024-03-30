a98b0864108b5812aca0b242aff6a08776ad33af
jfmoe <taoxfan@outlook.com>
2024-03-30 10:52:36 +0800

    fix: handle long text in listitem

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { CategoryItem } from './app/components/AppPicker';
 import LoginScreen from './app/screens/LoginScreen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <ListingDetailsScreen />;
 }
--- app/components/AppText.tsx
+++ app/components/AppText.tsx
@@ -1,14 +1,18 @@
 import { ReactNode } from 'react';
-import { Text } from 'react-native';
+import { Text, TextProps } from 'react-native';
 import defaultStyles from '../config/styles';
 
-interface Props {
+interface Props extends TextProps {
   children: ReactNode;
   style?: Object;
 }
 
-const AppText = ({ children, style }: Props) => {
-  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
+const AppText = ({ children, style, ...otherProps }: Props) => {
+  return (
+    <Text {...otherProps} style={[defaultStyles.text, style]}>
+      {children}
+    </Text>
+  );
 };
 
 export default AppText;
